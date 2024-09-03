eb0991af90320a9fa5bbf5a1a3eab0c7ab3b0c5c
Obaid Naseer <114626331+obaidnaseer@users.noreply.github.com>
2024-09-04 02:13:57 +0500

    Add text markup

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,7 @@ function App() {
           Hello World!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     </>
   )
 }
 
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,10 +1,10 @@
 function Hello() {
   return (
-    <>
-      <div>
-  <h1>Testing react components and JSX.</h1>
-      </div>
-  </>
+        <>
+    <h2>Adding Components is Easy</h2>
+    <p>There are mutliple parts to take care of, but it's <b>easy</b> to create React components.
+      <br /><br /> <i>Editing</i> those components &quot;once created&quot; is <b>easier!</b></p>
+      </>
   )
 }
 
